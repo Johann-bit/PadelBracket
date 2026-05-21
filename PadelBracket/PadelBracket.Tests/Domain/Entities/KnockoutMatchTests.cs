735bf317e7442a8cbdfd4b01ab1141de2685dde7
Johann <k3i0umaster@gmail.com>
2026-05-21 08:13:10 -0300

    Add knockout match entity

---
+++ PadelBracket/PadelBracket.Tests/Domain/Entities/KnockoutMatchTests.cs
@@ -0,0 +1,151 @@
+﻿using PadelBracket.Domain.Entities;
+
+namespace PadelBracket.Tests.Domain.Entities;
+
+[TestClass]
+public class KnockoutMatchTests
+{
+    [TestMethod]
+    public void Constructor_WhenRoundNameIsEmpty_ThrowsArgumentException()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+        var pairTwo = CreatePair("Nico", "Santi");
+
+        Assert.ThrowsException<ArgumentException>(() =>
+            new KnockoutMatch("", pairOne, pairTwo)
+        );
+    }
+
+    [TestMethod]
+    public void Constructor_WhenDataIsValid_CreatesKnockoutMatch()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+        var pairTwo = CreatePair("Nico", "Santi");
+
+        var match = new KnockoutMatch("Semifinal", pairOne, pairTwo);
+
+        Assert.AreNotEqual(Guid.Empty, match.Id);
+        Assert.AreEqual("Semifinal", match.RoundName);
+        Assert.AreEqual(pairOne.Id, match.PairOne!.Id);
+        Assert.AreEqual(pairTwo.Id, match.PairTwo!.Id);
+    }
+
+    [TestMethod]
+    public void IsReadyToPlay_WhenBothPairsAreAssigned_ReturnsTrue()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+        var pairTwo = CreatePair("Nico", "Santi");
+
+        var match = new KnockoutMatch("Semifinal", pairOne, pairTwo);
+
+        Assert.IsTrue(match.IsReadyToPlay);
+    }
+
+    [TestMethod]
+    public void IsReadyToPlay_WhenOnePairIsMissing_ReturnsFalse()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+
+        var match = new KnockoutMatch("Final", pairOne, null);
+
+        Assert.IsFalse(match.IsReadyToPlay);
+    }
+
+    [TestMethod]
+    public void RegisterResult_WhenMatchIsNotReady_ThrowsInvalidOperationException()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+        var match = new KnockoutMatch("Final", pairOne, null);
+
+        var result = CreatePairOneWinResult();
+
+        Assert.ThrowsException<InvalidOperationException>(() =>
+            match.RegisterResult(result)
+        );
+    }
+
+    [TestMethod]
+    public void RegisterResult_WhenMatchIsReady_SavesResult()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+        var pairTwo = CreatePair("Nico", "Santi");
+
+        var match = new KnockoutMatch("Semifinal", pairOne, pairTwo);
+
+        match.RegisterResult(CreatePairOneWinResult());
+
+        Assert.IsTrue(match.HasResult);
+        Assert.IsNotNull(match.Result);
+    }
+
+    [TestMethod]
+    public void Winner_WhenPairOneWins_ReturnsPairOne()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+        var pairTwo = CreatePair("Nico", "Santi");
+
+        var match = new KnockoutMatch("Semifinal", pairOne, pairTwo);
+
+        match.RegisterResult(CreatePairOneWinResult());
+
+        Assert.IsNotNull(match.Winner);
+        Assert.AreEqual(pairOne.Id, match.Winner!.Id);
+    }
+
+    [TestMethod]
+    public void Loser_WhenPairOneWins_ReturnsPairTwo()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+        var pairTwo = CreatePair("Nico", "Santi");
+
+        var match = new KnockoutMatch("Semifinal", pairOne, pairTwo);
+
+        match.RegisterResult(CreatePairOneWinResult());
+
+        Assert.IsNotNull(match.Loser);
+        Assert.AreEqual(pairTwo.Id, match.Loser!.Id);
+    }
+
+    [TestMethod]
+    public void AssignPairOne_WhenPairIsValid_AssignsPairOne()
+    {
+        var pairOne = CreatePair("Juan", "Pedro");
+
+        var match = new KnockoutMatch("Final", null, null);
+
+        match.AssignPairOne(pairOne);
+
+        Assert.IsNotNull(match.PairOne);
+        Assert.AreEqual(pairOne.Id, match.PairOne!.Id);
+    }
+
+    [TestMethod]
+    public void AssignPairTwo_WhenPairIsValid_AssignsPairTwo()
+    {
+        var pairTwo = CreatePair("Nico", "Santi");
+
+        var match = new KnockoutMatch("Final", null, null);
+
+        match.AssignPairTwo(pairTwo);
+
+        Assert.IsNotNull(match.PairTwo);
+        Assert.AreEqual(pairTwo.Id, match.PairTwo!.Id);
+    }
+
+    private static Pair CreatePair(string playerOneName, string playerTwoName)
+    {
+        var playerOne = new Player(playerOneName);
+        var playerTwo = new Player(playerTwoName);
+
+        return new Pair(playerOne, playerTwo);
+    }
+
+    private static MatchResult CreatePairOneWinResult()
+    {
+        return new MatchResult(new List<MatchSet>
+        {
+            new MatchSet(6, 3),
+            new MatchSet(6, 4)
+        });
+    }
+}
---
+++ PadelBracket/PadelBracket/Domain/Entities/KnockoutMatch.cs
@@ -0,0 +1,66 @@
+﻿namespace PadelBracket.Domain.Entities;
+
+public class KnockoutMatch
+{
+    public Guid Id { get; private set; }
+    public string RoundName { get; private set; }
+    public Pair? PairOne { get; private set; }
+    public Pair? PairTwo { get; private set; }
+    public MatchResult? Result { get; private set; }
+
+    public KnockoutMatch(string roundName, Pair? pairOne, Pair? pairTwo)
+    {
+        if (string.IsNullOrWhiteSpace(roundName))
+            throw new ArgumentException("Round name is required.");
+
+        Id = Guid.NewGuid();
+        RoundName = roundName.Trim();
+        PairOne = pairOne;
+        PairTwo = pairTwo;
+    }
+
+    public bool HasPairOne => PairOne is not null;
+    public bool HasPairTwo => PairTwo is not null;
+    public bool IsReadyToPlay => HasPairOne && HasPairTwo;
+    public bool HasResult => Result is not null;
+
+    public Pair? Winner
+    {
+        get
+        {
+            if (Result is null || PairOne is null || PairTwo is null)
+                return null;
+
+            return Result.PairOneWon ? PairOne : PairTwo;
+        }
+    }
+
+    public Pair? Loser
+    {
+        get
+        {
+            if (Result is null || PairOne is null || PairTwo is null)
+                return null;
+
+            return Result.PairOneWon ? PairTwo : PairOne;
+        }
+    }
+
+    public void RegisterResult(MatchResult result)
+    {
+        if (!IsReadyToPlay)
+            throw new InvalidOperationException("Cannot register a result before both pairs are assigned.");
+
+        Result = result ?? throw new ArgumentNullException(nameof(result));
+    }
+
+    public void AssignPairOne(Pair pair)
+    {
+        PairOne = pair ?? throw new ArgumentNullException(nameof(pair));
+    }
+
+    public void AssignPairTwo(Pair pair)
+    {
+        PairTwo = pair ?? throw new ArgumentNullException(nameof(pair));
+    }
+}
