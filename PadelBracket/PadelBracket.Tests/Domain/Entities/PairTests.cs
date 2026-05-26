9d9f2d7a51be274ae000ed35ca5368d4e5ec3f30
Johann <k3i0umaster@gmail.com>
2026-05-25 21:07:26 -0300

    [GREEN] Add Player and Pair entity tests

---
+++ PadelBracket/PadelBracket.Tests/Domain/Entities/PairTests.cs
@@ -0,0 +1,146 @@
+﻿using PadelBracket.Domain.Entities;
+
+namespace PadelBracket.Tests.Domain.Entities;
+
+[TestClass]
+public class PairTests
+{
+    [TestMethod]
+    public void Constructor_WithTwoDifferentPlayers_ShouldCreatePair()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+
+        Pair pair = new Pair(playerOne, playerTwo);
+
+        Assert.AreNotEqual(Guid.Empty, pair.Id);
+        Assert.AreEqual(playerOne, pair.PlayerOne);
+        Assert.AreEqual(playerTwo, pair.PlayerTwo);
+    }
+
+    [TestMethod]
+    public void Constructor_WithNullPlayerOne_ShouldThrowArgumentNullException()
+    {
+        Player playerTwo = new Player("Franco");
+
+        Assert.ThrowsException<ArgumentNullException>(() => new Pair(null!, playerTwo));
+    }
+
+    [TestMethod]
+    public void Constructor_WithNullPlayerTwo_ShouldThrowArgumentNullException()
+    {
+        Player playerOne = new Player("Johann");
+
+        Assert.ThrowsException<ArgumentNullException>(() => new Pair(playerOne, null!));
+    }
+
+    [TestMethod]
+    public void Constructor_WithSamePlayerTwice_ShouldThrowArgumentException()
+    {
+        Player player = new Player("Johann");
+
+        Assert.ThrowsException<ArgumentException>(() => new Pair(player, player));
+    }
+
+    [TestMethod]
+    public void DisplayName_ShouldReturnPlayerOneAndPlayerTwoNames()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+
+        string displayName = pair.DisplayName;
+
+        Assert.AreEqual("Johann / Franco", displayName);
+    }
+
+    [TestMethod]
+    public void RenamePlayers_WithValidNames_ShouldRenameBothPlayers()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+
+        pair.RenamePlayers("Bruno", "Joshua");
+
+        Assert.AreEqual("Bruno", pair.PlayerOne.Name);
+        Assert.AreEqual("Joshua", pair.PlayerTwo.Name);
+        Assert.AreEqual("Bruno / Joshua", pair.DisplayName);
+    }
+
+    [TestMethod]
+    public void RenamePlayers_WithNamesContainingExtraSpaces_ShouldTrimBothNames()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+
+        pair.RenamePlayers("  Bruno  ", "  Joshua  ");
+
+        Assert.AreEqual("Bruno", pair.PlayerOne.Name);
+        Assert.AreEqual("Joshua", pair.PlayerTwo.Name);
+        Assert.AreEqual("Bruno / Joshua", pair.DisplayName);
+    }
+
+    [TestMethod]
+    public void RenamePlayers_WithNullPlayerOneName_ShouldThrowArgumentException()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+        string? playerOneName = null;
+
+        Assert.ThrowsException<ArgumentException>(() => pair.RenamePlayers(playerOneName!, "Joshua"));
+    }
+
+    [TestMethod]
+    public void RenamePlayers_WithNullPlayerTwoName_ShouldThrowArgumentException()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+        string? playerTwoName = null;
+
+        Assert.ThrowsException<ArgumentException>(() => pair.RenamePlayers("Bruno", playerTwoName!));
+    }
+
+    [TestMethod]
+    public void RenamePlayers_WithEmptyPlayerOneName_ShouldThrowArgumentException()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+
+        Assert.ThrowsException<ArgumentException>(() => pair.RenamePlayers("", "Joshua"));
+    }
+
+    [TestMethod]
+    public void RenamePlayers_WithEmptyPlayerTwoName_ShouldThrowArgumentException()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+
+        Assert.ThrowsException<ArgumentException>(() => pair.RenamePlayers("Bruno", ""));
+    }
+
+    [TestMethod]
+    public void RenamePlayers_WithWhiteSpacePlayerOneName_ShouldThrowArgumentException()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+
+        Assert.ThrowsException<ArgumentException>(() => pair.RenamePlayers("   ", "Joshua"));
+    }
+
+    [TestMethod]
+    public void RenamePlayers_WithWhiteSpacePlayerTwoName_ShouldThrowArgumentException()
+    {
+        Player playerOne = new Player("Johann");
+        Player playerTwo = new Player("Franco");
+        Pair pair = new Pair(playerOne, playerTwo);
+
+        Assert.ThrowsException<ArgumentException>(() => pair.RenamePlayers("Bruno", "   "));
+    }
+}
---
+++ PadelBracket/PadelBracket.Tests/Domain/Entities/PlayerTests.cs
@@ -0,0 +1,97 @@
+﻿using PadelBracket.Domain.Entities;
+
+namespace PadelBracket.Tests.Domain.Entities;
+
+[TestClass]
+public class PlayerTests
+{
+    [TestMethod]
+    public void Constructor_WithValidName_ShouldCreatePlayer()
+    {
+        string name = "Johann";
+
+        Player player = new Player(name);
+
+        Assert.AreNotEqual(Guid.Empty, player.Id);
+        Assert.AreEqual("Johann", player.Name);
+    }
+
+    [TestMethod]
+    public void Constructor_WithNullName_ShouldThrowArgumentException()
+    {
+        string? name = null;
+
+        Assert.ThrowsException<ArgumentException>(() => new Player(name!));
+    }
+
+    [TestMethod]
+    public void Constructor_WithEmptyName_ShouldThrowArgumentException()
+    {
+        string name = "";
+
+        Assert.ThrowsException<ArgumentException>(() => new Player(name));
+    }
+
+    [TestMethod]
+    public void Constructor_WithWhiteSpaceName_ShouldThrowArgumentException()
+    {
+        string name = "   ";
+
+        Assert.ThrowsException<ArgumentException>(() => new Player(name));
+    }
+
+    [TestMethod]
+    public void Constructor_WithNameContainingExtraSpaces_ShouldTrimName()
+    {
+        string name = "  Johann  ";
+
+        Player player = new Player(name);
+
+        Assert.AreEqual("Johann", player.Name);
+    }
+
+    [TestMethod]
+    public void Rename_WithValidName_ShouldChangePlayerName()
+    {
+        Player player = new Player("Johann");
+
+        player.Rename("Franco");
+
+        Assert.AreEqual("Franco", player.Name);
+    }
+
+    [TestMethod]
+    public void Rename_WithNameContainingExtraSpaces_ShouldTrimName()
+    {
+        Player player = new Player("Johann");
+
+        player.Rename("  Franco  ");
+
+        Assert.AreEqual("Franco", player.Name);
+    }
+
+    [TestMethod]
+    public void Rename_WithNullName_ShouldThrowArgumentException()
+    {
+        Player player = new Player("Johann");
+        string? newName = null;
+
+        Assert.ThrowsException<ArgumentException>(() => player.Rename(newName!));
+    }
+
+    [TestMethod]
+    public void Rename_WithEmptyName_ShouldThrowArgumentException()
+    {
+        Player player = new Player("Johann");
+
+        Assert.ThrowsException<ArgumentException>(() => player.Rename(""));
+    }
+
+    [TestMethod]
+    public void Rename_WithWhiteSpaceName_ShouldThrowArgumentException()
+    {
+        Player player = new Player("Johann");
+
+        Assert.ThrowsException<ArgumentException>(() => player.Rename("   "));
+    }
+}
